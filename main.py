02559a0b06825ccbb7b36ed548ad7620e6bb275e
Bvceh228 <148950537+Bvceh228@users.noreply.github.com>
2024-12-22 14:02:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 app = FastAPI()
+
+origins = [
+    https://931e0d09-537e-4784-a846-86956217271d-00-1uqhgnli0hcjv.worf.replit.dev:3001
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Pizza(BaseModel):
     name: str
     description: str
     ingredients: list[str]
     price: float
+    
 pizzas = [
     Pizza(name="Pepperoni", description="A classic pizza with pepperoni", ingredients=['1', '2', '3'], price=10.0), 
     Pizza(name="Pepperoni2", description="A classic pizza with pepperoni2", ingredients=['1', '2', '3'], price=12.0)
