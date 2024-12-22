e02342f6c352e0c282e803d381d7c4c82e279d8a
Bvceh228 <148950537+Bvceh228@users.noreply.github.com>
2024-12-22 14:06:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 app = FastAPI()
 
+# Допустимые источники (домены), с которых ожидаются запросы
 origins = [
-    https://931e0d09-537e-4784-a846-86956217271d-00-1uqhgnli0hcjv.worf.replit.dev:3001
+    "https://931e0d09-537e-4784-a846-86956217271d-00-1uqhgnli0hcjv.worf.replit.dev:3001"
 ]
 
 app.add_middleware(
@@ -20,14 +22,16 @@ class Pizza(BaseModel):
     description: str
     ingredients: list[str]
     price: float
-    
+
 pizzas = [
-    Pizza(name="Pepperoni", description="A classic pizza with pepperoni", ingredients=['1', '2', '3'], price=10.0), 
+    Pizza(name="Pepperoni", description="A classic pizza with pepperoni", ingredients=['1', '2', '3'], price=10.0),
     Pizza(name="Pepperoni2", description="A classic pizza with pepperoni2", ingredients=['1', '2', '3'], price=12.0)
 ]
+
 @app.get("/")
 async def root():
     return pizzas
+
 @app.get("/pizzas/{pizza_name}")
 async def get_pizza(pizza_name: str):
     for pizza in pizzas:
